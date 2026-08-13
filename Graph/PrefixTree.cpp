3f5e611ff48d8fe0d7134fde5fb8b28547b46846
athreeman <2351241176@qq.com>
2026-08-13 19:07:02 +0800

    Refactor PrefixTree implementation for clarity

--- Graph/PrefixTree.cpp
+++ Graph/PrefixTree.cpp
@@ -8,105 +8,109 @@
 using std::string;
 using std::fill;
 
-const int MAXN=1e6;
-int tree[MAXN+1][26];//'a'~'z',0~25
-int pass[MAXN+1];
-int end[MAXN+1];
-int cnt=1;
+const int MAXN = 1e6;
+int tree[MAXN + 1][26];//'a'~'z',0~25
+int pass[MAXN + 1];//多少个字符串经过该点
+int end[MAXN + 1];//多少个字符串以该点结尾
+int cnt = 1;//节点编号,申请和撤销空间
 
-void build(){
-    cnt=1;//1作为头节点
+void build() {
+    cnt = 1;//1作为头节点
 }
 
-void insert(string word){
-    int cur=1;//cur初始化1,头节点
+void insert(string word) {
+    int cur = 1;//cur初始化1,头节点
     pass[cur]++;
-    for(int i=0;i<word.size();i++){
-        int path=word[i]-'a';
-        if(tree[cur][path]==0){
-            tree[cur][path]=++cnt;
+    for (int i = 0;i < word.size();i++) {
+        int path = word[i] - 'a';
+        if (tree[cur][path] == 0) {
+            tree[cur][path] = ++cnt;
         }
-        cur=tree[cur][path];
+        cur = tree[cur][path];
         pass[cur]++;
     }
     end[cur]++;
 }
 
-int search(string word){
-    int cur=1;
-    for(int i=0;i<word.size();i++){
-        int path=word[i]-'a';
-        if(tree[cur][path]==0){
+int search(string word) {
+    int cur = 1;
+    for (int i = 0;i < word.size();i++) {
+        int path = word[i] - 'a';
+        if (tree[cur][path] == 0) {
             return 0;
         }
-        cur=tree[cur][path];
+        cur = tree[cur][path];
     }
     return end[cur];
 }
 
-int countPrefix(string word){
-    int cur=1;
-    for(int i=0;i<word.size();i++){
-        int path=word[i]-'a';
-        if(tree[cur][path]==0){
+int countPrefix(string word) {
+    int cur = 1;
+    for (int i = 0;i < word.size();i++) {
+        int path = word[i] - 'a';
+        if (tree[cur][path] == 0) {
             return 0;
         }
-        cur=tree[cur][path];
+        cur = tree[cur][path];
     }
     return pass[cur];
 }
 
-void erase(string word){
-    if(search(word)<1)return;
-    int cur=1;
+void erase(string word) {
+    if (search(word) < 1)return;
+    int cur = 1;
     //沿途pass--
     //终点end--
-    for(int i=0;i<word.size();i++){
-        int path=word[i]-'a';
-        if(--pass[tree[cur][path]]==0){
+    for (int i = 0;i < word.size();i++) {
+        int path = word[i] - 'a';
+        if (--pass[tree[cur][path]] == 0) {
             //全部删除完毕,清楚节点编号
-            tree[cur][path]=0;
+            tree[cur][path] = 0;
             return;
         }
-        cur=tree[cur][path];
+        cur = tree[cur][path];
     }
     end[cur]--;
 }
 
-void clear(){
-    for(int i=1;i<=cnt;i++){
-        fill(tree[i],tree[i]+26,0);
-        pass[i]=end[i]=0;
+void clear() {
+    for (int i = 1;i <= cnt;i++) {
+        fill(tree[i], tree[i] + 26, 0);
+        pass[i] = end[i] = 0;
     }
 }
 
-int main(){
+int main() {
     using namespace std;
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t=1;
+    int t = 1;
     //cin>>t;
-    while(t--){
+    while (t--) {
         int n;
-        cin>>n;
+        cin >> n;
         build();
-        while(n--){
+        while (n--) {
             int opt;
             string word;
-            cin>>opt>>word;
-            if(opt==1){
+            cin >> opt >> word;
+            if (opt == 1) {
                 insert(word);
-            }else if(opt==2){
+            }
+            else if (opt == 2) {
                 erase(word);
-            }else if(opt==3){
-                if(search(word)>0){
-                    cout<<"YES"<<'\n';
-                }else{
-                    cout<<"NO"<<'\n';
+            }
+            else if (opt == 3) {
+                if (search(word) > 0) {
+                    cout << "YES" << '\n';
                 }
-            }else{
-                cout<<countPrefix(word)<<'\n';
+                else {
+                    cout << "NO" << '\n';
+                }
+            }
+            else {
+                cout << countPrefix(word) << '\n';
             }
         }
         clear();
