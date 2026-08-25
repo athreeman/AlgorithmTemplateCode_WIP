f3427d49845d105a0e5639e9f3e2f8e2b0e8750f
athreeman <2351241176@qq.com>
2026-08-25 19:20:35 +0800

    Add common namespace with standard library using directives

---
+++ 常用一般命名空间.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+namespace name {
+    using std::cout;
+    using std::cin;
+    using std::swap;
+    using std::fill;
+    using std::endl;
+    using std::max;
+    using std::min;
+    using std::abs;
+
+    using std::vector;
+    using std::string;
+    using std::pair;
+    using std::array;
+    using std::deque;
+    using std::list;
+    using std::stack;
+    using std::queue;
+    using std::priority_queue;
+    using std::set;
+    using std::multiset;
+    using std::map;
+    using std::multimap;
+    using std::unordered_set;
+    using std::unordered_multiset;
+    using std::unordered_map;
+    using std::unordered_multimap;
+    using std::bitset;
+
+    using std::sort;
+    using std::reverse;
+    using std::unique;
+    using std::lower_bound;
+    using std::upper_bound;
+
+
+    using std::sqrt;
+    using std::log;
+    using std::log2;
+    using std::log10;
+    using std::ceil;
+    using std::floor;
+    using std::round;
+
+    using ll = long long;
+    using ull = unsigned long long;
+    using ld = long double;
+    using ill = __int128_t;
+
+}
