1df6c9c08a9d3fdc75ca2e1b2982577218c481ef
athreeman <2351241176@qq.com>
2026-08-25 21:08:53 +0800

    Add getNthDigit function for base conversion
    Add a function to retrieve the i-th digit of a number in a given base, with a note on potential overflow for high bases.

---
+++ 数学/进制转换.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const string DIGIT =
+"0123456789"
+"abcdefghijklmnopqrstuvwxyz"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+//该DIGIT字符最多支持94进制的转换
+//如果仅限数字和字母最多支持62进制的转换
+
+//把x转换为base进制
+string toBase(ll x, int base) {
+    if (x == 0) {
+        return "0";
+    }
+    string ans;
+    while (x) {
+        ans += DIGIT[x % base];
+        x /= base;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+//快速幂
+ll pw(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) {
+            res = res * a;
+        }
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//获取x在base进制下的第i位
+//注意！：高进制有越界风险
+ll getNthDigit(ll x, int base, int i) {
+    return x / pw(base, i) % base;
+}
