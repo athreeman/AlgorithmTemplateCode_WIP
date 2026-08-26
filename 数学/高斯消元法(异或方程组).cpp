a4887b4b8d0e0505599f0ba0d272502f4d38ef12
athreeman <2351241176@qq.com>
2026-08-26 16:29:01 +0800

    Remove failure comment in Gaussian elimination
    Removed comment about failure in finding a pivot.

--- 数学/高斯消元法(异或方程组).cpp
+++ 数学/高斯消元法(异或方程组).cpp
@@ -37,7 +37,7 @@ void gauss(int n) {
                 break;
             }
         }
-        // 查找失败
+
         if (mat[i][i] == 1) {
             for (int j = 1;j <= n;j++) {
                 if (i != j && mat[j][i] == 1) {
