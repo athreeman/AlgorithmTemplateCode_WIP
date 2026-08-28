a9ef7578322c577a80e9dc20437b46511d45931f
athreeman <2351241176@qq.com>
2026-08-28 13:22:55 +0800

    Improve comments and structure in monotonic stack code
    Refactor monotonic stack implementation to clarify the purpose and improve comments.

--- 基础算法/单调栈.cpp
+++ 基础算法/单调栈.cpp
@@ -1,18 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 单调栈求解左侧和右侧第一个小于的位置
+
 // 单调栈的应用很广泛：
 // 贪心, dp优化, 很多场景都可以应用
+
+
+// 模板：
+// 单调栈求解左侧和右侧第一个严格小于的位置
 stack<int>sta;
 const int MAXN = 2e5;
 int right[MAXN + 1];
 int left[MAXN + 1];
 int a[MAXN + 1];
 void solve(int n) {
+    // -1标记不存在
     // 栈必须维护栈顶是最大值
     for (int i = 1;i <= n;i++) {
-
         // 右侧在弹出时进行维护
         while (sta.size() && a[i] < a[sta.top()]) {
             int t = sta.top();
@@ -30,6 +34,9 @@ void solve(int n) {
                 ::left[i] = sta.top();
             }
         }
+        else {
+            ::left[i] = -1;
+        }
         sta.push(i);
     }
     while (sta.size()) {
@@ -37,3 +44,4 @@ void solve(int n) {
         ::right[t] = -1;
         sta.pop();
     }
+}
