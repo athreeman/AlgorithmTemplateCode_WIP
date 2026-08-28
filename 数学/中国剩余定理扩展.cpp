2de2734cd1cc021a018b8e2f1776f1c891c3c501
athreeman <2351241176@qq.com>
2026-08-28 18:15:57 +0800

    Add extended Chinese Remainder Theorem implementation
    Implement extended Chinese Remainder Theorem for non-coprime moduli.

---
+++ 数学/中国剩余定理扩展.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ill = __int128_t;
+
+// 中国剩余定理
+// 扩展：m1, m2, m3, ..., mn之间不需要严格互质
+// x% m1 = r1, x% m2 = r2, ..., x% mn = rn
+// 补充初始模数m0 = 1, lcm = 1, tail = 0, 得到ans1 = x * lcm + tail
+// 对于mi, ri, 得到ans2 = y * mi + ri
+// 显然ans1 = ans2, 相消得到lcm* x + mi * y = ri - tail, 即ax + by = c
+// exgcd求解, 若不存在解说明无解
+// 通解x = x0 + (b / d) * n, 带入ans = lcm * x + tail
+// ans = lcm * (b / d) * n + (lcm * x0 + tail)
+// 得到lcm1 = lcm * (b / d), tail1 = (lcm * x0 + tail) % lcm1
+// 得到ans = lcm1 * x + tail1
+// 持续迭代直到结束, 得到最后的tail就是答案(最小整数解)
+
+const int MAXN = 2e5;
+ll r[MAXN + 1];
+ll m[MAXN + 1];
+
+ll add(ll a, ll b, ll mod) {
+    return b == 0 ? a : add(a ^ b, ((a & b) << 1) % mod, mod);
+}
+ll multiply(ll a, ll b, ll ans, ll mod) {
+    return b == 0 ? ans : multiply((a << 1) % mod, (ull)b >> 1, (b & 1) ? add(ans, a, mod) : ans, mod);
+}
+ll mul(ll a, ll b, ll mod) { return multiply(a, b, 0, mod); }
+
+
+ll exgcd(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    else {
+        ll g = exgcd(b, a % b, x, y);
+        ll tmp = y;
+        y = x - y * (a / b);
+        x = tmp;
+        return g;
+    }
+}
+
+// 模板：https://www.luogu.com.cn/problem/P4777
+// C++建议使用__int128_t类型, 比位运算实现更为安全和高效
+ll excrt(int n) {
+    ll tail = 0, lcm = 1, tmp, b, c, x0;
+    for (int i = 1;i <= n;i++) {
+        b = m[i];
+        c = ((r[i] - tail) % b + b) % b;
+        ll x, y;
+        ll d = exgcd(lcm, b, x, y);
+        if (c % d != 0) {
+            return -1;//无解
+        }
+        // x0保证非负特解
+        x0 = ((ill)x * (ill)(c / d) % (b / d) + ill(b / d)) % (b / d);
+        tmp = lcm * (b / d);
+        tail = ((ill)tail + ill(x0) * ill(lcm) % tmp) % tmp;
+        lcm = tmp;
+    }
+    return tail;
+}
