f15fc6251a4d89c5c589474752414cbb82af8907
athreeman <2351241176@qq.com>
2026-08-28 13:20:12 +0800

    Enhance monotonic stack to find left and right smaller
    Refactor the monotonic stack algorithm to find both left and right first smaller positions. Added handling for equal elements and improved code clarity.

--- 基础算法/单调栈.cpp
+++ 基础算法/单调栈.cpp
@@ -1,25 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 单调栈求解右侧第一个小的位置
+// 单调栈求解左侧和右侧第一个小于的位置
 // 单调栈的应用很广泛：
 // 贪心, dp优化, 很多场景都可以应用
 stack<int>sta;
 const int MAXN = 2e5;
 int right[MAXN + 1];
+int left[MAXN + 1];
 int a[MAXN + 1];
 void solve(int n) {
     // 栈必须维护栈顶是最大值
     for (int i = 1;i <= n;i++) {
+
+        // 右侧在弹出时进行维护
         while (sta.size() && a[i] < a[sta.top()]) {
-            ::right[sta.top()] = i;
+            int t = sta.top();
+            ::right[t] = i;
             sta.pop();
+        }
+
+        // 左侧在压入时进行维护
+        // 若存在相同元素, 和左侧第一个相同位置对齐
+        if (sta.size()) {
+            if (a[i] == a[sta.top()]) {
+                ::left[i] = ::left[sta.top()];
+            }
+            else {
+                ::left[i] = sta.top();
+            }
         }
         sta.push(i);
     }
-    // 标记不存在
     while (sta.size()) {
-        ::right[sta.top()] = -1;
+        int t = sta.top();
+        ::right[t] = -1;
         sta.pop();
     }
-}
