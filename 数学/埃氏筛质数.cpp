3f761744dc20369cb2b240483dacdaef25cb92e6
athreeman <2351241176@qq.com>
2026-09-01 21:33:07 +0800

    Refactor Eratosthenes sieve for prime generation
    Refactor Eratosthenes sieve implementation to use arrays for prime storage and minimum factors.

--- 数学/埃氏筛质数.cpp
+++ 数学/埃氏筛质数.cpp
@@ -2,19 +2,25 @@
 using namespace std;
 using ll = long long;
 const int MAXN = 1e7;
-bitset<2 * MAXN + 1>isPrime;//标记质数
-vector<ll>Primes;//存储质数
+bitset<MAXN + 1>isPrime;//标记质数
+ll Primes[MAXN + 1];
+ll minFactor[MAXN + 1];
 void getPrimes_Eratosthenes(ll n) {
     //埃氏筛
     //复杂度O(n*log2(log2n))
     //获取1~n的所有质数
+    Primes[0] = 0;
     isPrime.set();
     isPrime[0] = isPrime[1] = 0;
     for (ll i = 2;i <= n;i++) {
         if (isPrime[i]) {
-            Primes.push_back(i);
+            minFactor[i] = i;
+            Primes[++Primes[0]] = i;
             for (ll j = i * i;j <= n;j += i) {
                 isPrime[j] = 0;
+                if (minFactor[j] == 0) {
+                    minFactor[j] = i;
+                }
             }
         }
     }
