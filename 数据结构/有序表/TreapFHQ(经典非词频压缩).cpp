853b8891f0b5f3f980d1e873832f0b09d6cdde9f
athreeman <2351241176@qq.com>
2026-09-04 21:41:14 +0800

    Add FHQTreap implementation in TreapFHQ.cpp
    Implement FHQTreap data structure with key-based splitting and priority-based merging. Includes methods for adding, removing, and querying elements.

---
+++ 数据结构/有序表/TreapFHQ(经典非词频压缩).cpp
@@ -0,0 +1,163 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// FHQTreap
+// 核心: 根据key值执行分裂, 根据优先级执行合并操作
+
+const int MAXN = 2e5;
+const ll INF = 1e18;
+int head = 0, cnt = 0;
+struct {
+    ll key;
+    int priority;
+    int left;
+    int right;
+    int size;// 节点总数
+}node[MAXN + 1];
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int>dist(1, INT_MAX);
+
+void clear() {
+    for (int i = 0;i <= cnt;i++) {
+        node[i] = { 0,0,0,0,0 };
+    }
+    head = cnt = 0;
+}
+
+void up(int i) {
+    node[i].size = node[node[i].left].size + node[node[i].right].size + 1;
+}
+
+// 若当前 curv <= key, 则左侧子树不需要改变, 向右子树递归
+// 反之, 右侧子树不需要改变, 向左子树递归
+
+// key值分裂
+void splitByKey(int l, int r, int i, ll key) {
+    if (i == 0) {
+        node[l].right = node[r].left = 0;
+    }
+    else {
+        if (node[i].key <= key) {
+            node[l].right = i;
+            splitByKey(i, r, node[i].right, key);
+        }
+        else {
+            node[r].left = i;
+            splitByKey(l, i, node[i].left, key);
+        }
+        up(i);
+    }
+}
+
+// 经典非词频压缩, 可正常使用
+void splitByRank(int l, int r, int i, ll rk) {
+    if (i == 0) {
+        node[l].right = node[r].left = 0;
+    }
+    else {
+        if (node[node[i].left].size + 1 <= rk) {
+            node[l].right = i;
+            splitByRank(i, r, node[i].right, rk - node[node[i].left].size - 1);
+        }
+        else {
+            node[r].left = i;
+            splitByRank(l, i, node[i].left, rk);
+        }
+        up(i);
+    }
+}
+
+int merge(int l, int r) {
+    if (l == 0 || r == 0) {
+        return l + r;
+    }
+    if (node[l].priority >= node[r].priority) {
+        node[l].right = merge(node[l].right, r);
+        up(l);
+        return l;
+    }
+    else {
+        node[r].left = merge(l, node[r].left);
+        up(r);
+        return r;
+    }
+}
+
+void add(ll val) {
+    splitByKey(0, 0, head, val);
+    node[++cnt].key = val;
+    node[cnt].size = 1;
+    node[cnt].priority = dist(gen);
+    head = merge(merge(node[0].right, cnt), node[0].left);
+}
+
+void remove(ll val) {
+    splitByKey(0, 0, head, val);
+    int lm = node[0].right;// <=val
+    int r = node[0].left;// >val
+    splitByKey(0, 0, lm, val - 1);
+    int l = node[0].right;// <=val-1
+    int m = node[0].left;// =val
+    // =val的去掉一个(头), val-1部分和>val部分全部保留
+    head = merge(merge(l, merge(node[m].left, node[m].right)), r);
+}
+
+int small(ll val) {
+    splitByKey(0, 0, head, val - 1);
+    int ans = node[node[0].right].size;
+    head = merge(node[0].right, node[0].left);
+    return ans;
+}
+
+int rank(ll val) {
+    return small(val) + 1;
+}
+
+ll index(int i, int x) {
+    if (node[node[i].left].size >= x) {
+        return index(node[i].left, x);
+    }
+    else if (node[node[i].left].size + 1 < x) {
+        return index(node[i].right, x - node[node[i].left].size - 1);
+    }
+    return node[i].key;
+}
+
+ll index(int x) {
+    return index(head, x);
+}
+
+ll pre(int i, ll val) {
+    if (i == 0) {
+        return -INF;
+    }
+    if (node[i].key >= val) {
+        return pre(node[i].left, val);
+    }
+    else {
+        return max(node[i].key, pre(node[i].right, val));
+    }
+}
+
+ll pre(ll val) {
+    return pre(head, val);
+}
+
+ll post(int i, ll val) {
+    if (i == 0) {
+        return INF;
+    }
+    if (node[i].key <= val) {
+        return post(node[i].right, val);
+    }
+    else {
+        return min(node[i].key, post(node[i].left, val));
+    }
+}
+
+ll post(ll val) {
+    return post(head, val);
+}
